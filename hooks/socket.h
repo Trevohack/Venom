571353dbdfa8f6560199e7b871acf2bae3426919
!0xTrev <136177431+Trevohack@users.noreply.github.com>
2025-10-08 17:20:27 +0530

    Venom - Hook socket

---
+++ hooks/socket.h
@@ -0,0 +1,21 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include "../include/headers.h"
+
+static asmlinkage long (*orig_socket)(const struct pt_regs *regs);
+
+notrace static asmlinkage long hooked_socket(const struct pt_regs *regs) {
+    int family = (int)regs->di;
+    int type = (int)regs->si;
+    
+
+    if (type == SOCK_RAW) {
+        SLOG_WARN("[VENOM] Socket created by PID %d UID %d",  current->pid, current_uid().val);
+    }
+    
+    return orig_socket(regs);
+} 
+
+#endif 
+
